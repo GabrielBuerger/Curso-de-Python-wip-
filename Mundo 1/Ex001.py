3be8703b8995accb45debba5d36bd7211db502a4
Gabriel Buerger Melo <158763081+GabrielBuerger@users.noreply.github.com>
2024-02-05 14:48:46 -0300

    segundo exercício

---
+++ Mundo 1/Ex001.py
@@ -0,0 +1,2 @@
+print('''\033[1;34;43m Olá mundo! \033[m\033[34m
+Este é o inicio do curso de Python!\033[m''')
---
+++ Mundo 1/Ex002.py
@@ -0,0 +1,2 @@
+nome = input('\033[35mQual o seu nome?\n\033[m')
+print('\033[35mPrazer em conhece-lo(a), {}!\033[m'.format(nome))
